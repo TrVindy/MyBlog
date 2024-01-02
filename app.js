ed734434aa7ec2212e8a8ccc620ab2849888b02b
Udani Vindhya <taecher.vindy@gmail.com>
2024-01-03 00:57:49 +0530

    Initial Commit

---
+++ app.js
@@ -0,0 +1,49 @@
+require("dotenv").config();
+
+const express =require ("express");
+const expressLayout =require ("express-ejs-layouts");
+const methodOverride = require ("method-override") ;
+const cookieParser = require('cookie-parser');
+const MongoStore =require('connect-mongo');
+
+const connectDB= require('./server/config/db');
+const { connect, mongo } = require("mongoose");
+const session = require("express-session");
+const { isActiveRoute } = require('./server/helpers/routeHelpers');
+
+const app = express();
+const port = 5000 || process.env.port;
+
+//connect DB
+connectDB();
+
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+app.use(cookieParser());
+app.use(methodOverride('_method'));
+
+app.use(session({
+    secret: 'Keyboard cat',
+    resave: false,
+    saveUninitialized: true,
+    store: MongoStore.create({
+        mongoUrl: process.env.MONGODB_URI
+    }),
+
+}));
+
+app.use(express.static("public"));
+
+//Template Engine
+app.use(expressLayout);
+app.set("layout", "./layouts/main");
+app.set("view engine", "ejs");
+
+app.locals.isActiveRoute = isActiveRoute; 
+
+app.use("/", require("./server/routes/main"));
+app.use("/", require("./server/routes/admin"));
+
+app.listen(port, ()=>{
+    console.log(`Listening on port ${port}`);
+});
---
+++ server/routes/main.js
@@ -0,0 +1,145 @@
+const express = require("express");
+const router = express.Router();
+const Post = require('../models/Post');
+
+
+/**
+     * GET
+     * Home
+ */
+// router.get('', async (req, res) => {
+//   const locals = {
+//     title: "NodeJs Blog",
+//     description: "Simple Blog created with NodeJs, Express & MongoDb."
+//   }
+
+//   try {
+//     const data = await Post.find();
+//     res.render('index', { locals, data });
+//   } catch (error) {
+//     console.log(error);
+//   }
+
+// });
+//Routes
+router.get("", async (req, res) => {
+
+    try {
+        const locals = {
+            title: "Node js blog",
+            description: "Simple Blog created with NodeJs,Express  & Mongo"
+        }
+
+        let perPage = 6;
+        let page = req.query.page || 1;
+
+        const data = await Post.aggregate([{ $sort: { createdAt: -1 } }])
+            .skip(perPage * page - perPage)
+            .limit(perPage)
+            .exec();
+        const count = await Post.countDocuments({});
+        const nextPage = parseInt(page) + 1;
+        const hasNextPage = nextPage <= Math.ceil(count / perPage);
+
+
+
+        res.render("index", {
+            locals,
+            data,
+            current: page,
+            nextPage: hasNextPage ? nextPage : null,
+            currentRoute: "/"
+        });
+
+    } catch (error) {
+        console.log(error);
+    }
+
+
+});
+
+
+/**
+     * GET
+     * Home
+ */
+router.get('/post/:id', async (req, res) => {
+    try {
+
+        let slug = req.params.id;
+
+        const data = await Post.findById({ _id: slug });
+
+        const locals = {
+            title: data.title,
+            description: "Simple Blog created with , Express & MongoDb."
+        }
+        res.render('post', { 
+            locals, 
+            data,
+            currentRoute: `post/${slug}`
+         });
+    } catch (error) {
+        console.log(error);
+    }
+
+});
+
+
+/**
+     * GET
+     * Post -SearchTerms
+     * 
+ */
+router.post('/search', async (req, res) => {
+    try {
+        const locals = {
+            title: "Search",
+            description: "Simple Blog created with NodeJs, Express & MongoDb."
+        }
+
+        let searchTerm = req.body.searchTerm;
+        const searchNoSpecialCharactor = searchTerm.replace(/[^a-zA-Z0-9]/g, "");
+
+        const data = await Post.find({
+            $or:[
+                { title: { $regex: new RegExp(searchNoSpecialCharactor, 'i')}}, 
+                { body: { $regex: new RegExp(searchNoSpecialCharactor, 'i')}}
+            ]
+        });
+        res.render("search",{
+            data,
+            locals
+        });
+    } catch (error) {
+        console.log(error);
+    }
+
+});
+
+
+
+router.get("/about", (req, res) => {
+    res.render("about",{
+
+        currentRoute: "/about"
+        
+    });
+});
+
+module.exports = router;
+
+
+/**
+ * function insertPostData(){
+    Post.insertMany([
+        {
+            title:"Building a Blog",
+            body: "This is the body text"
+        }
+    ])
+}
+
+insertPostData();
+
+ */
